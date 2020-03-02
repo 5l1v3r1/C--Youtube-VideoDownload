54249244a9804774b0d201b22597073bfbd11812
Osman Yavuz <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-03-02 03:45:13 +0300

    Update

---
+++ YoutubeVideoDownload/YoutubeVideoDownload/Form_Main.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace YoutubeVideoDownload
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Download = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_URL = new System.Windows.Forms.TextBox();
+            this.label_Durum = new System.Windows.Forms.Label();
+            this.progressBar_Down = new System.Windows.Forms.ProgressBar();
+            this.listBox_Videolar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button_Download
+            // 
+            this.button_Download.Location = new System.Drawing.Point(300, 34);
+            this.button_Download.Name = "button_Download";
+            this.button_Download.Size = new System.Drawing.Size(75, 23);
+            this.button_Download.TabIndex = 0;
+            this.button_Download.Text = "Video Bilgisi";
+            this.button_Download.UseVisualStyleBackColor = true;
+            this.button_Download.Click += new System.EventHandler(this.button_Download_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Video Link";
+            // 
+            // textBox_URL
+            // 
+            this.textBox_URL.Location = new System.Drawing.Point(31, 36);
+            this.textBox_URL.Name = "textBox_URL";
+            this.textBox_URL.Size = new System.Drawing.Size(263, 20);
+            this.textBox_URL.TabIndex = 2;
+            this.textBox_URL.Text = "https://www.youtube.com/watch?v=Eo_nV-Yrg0s";
+            // 
+            // label_Durum
+            // 
+            this.label_Durum.Location = new System.Drawing.Point(28, 72);
+            this.label_Durum.Name = "label_Durum";
+            this.label_Durum.Size = new System.Drawing.Size(347, 39);
+            this.label_Durum.TabIndex = 3;
+            this.label_Durum.Text = "Bekleniyor..";
+            // 
+            // progressBar_Down
+            // 
+            this.progressBar_Down.Location = new System.Drawing.Point(31, 114);
+            this.progressBar_Down.Name = "progressBar_Down";
+            this.progressBar_Down.Size = new System.Drawing.Size(344, 23);
+            this.progressBar_Down.TabIndex = 4;
+            // 
+            // listBox_Videolar
+            // 
+            this.listBox_Videolar.FormattingEnabled = true;
+            this.listBox_Videolar.Location = new System.Drawing.Point(31, 143);
+            this.listBox_Videolar.Name = "listBox_Videolar";
+            this.listBox_Videolar.Size = new System.Drawing.Size(344, 199);
+            this.listBox_Videolar.TabIndex = 5;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 420);
+            this.Controls.Add(this.listBox_Videolar);
+            this.Controls.Add(this.progressBar_Down);
+            this.Controls.Add(this.label_Durum);
+            this.Controls.Add(this.textBox_URL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Download);
+            this.Name = "Form_Main";
+            this.Text = "Youtube Video Download";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Download;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_URL;
+        private System.Windows.Forms.Label label_Durum;
+        private System.Windows.Forms.ProgressBar progressBar_Down;
+        private System.Windows.Forms.ListBox listBox_Videolar;
+    }
+}
+
---
+++ YoutubeVideoDownload/YoutubeVideoDownload/Form_Main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YoutubeVideoDownload
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        string FilterFormat = "mp4";
+        string filterKalite = "1080";
+
+
+        private async void button_Download_Click(object sender, EventArgs e)
+        {
+            
+
+            listBox_Videolar.Items.Clear();
+
+
+            
+
+            var youtube = YouTube.Default;
+            var videos = await youtube.GetAllVideosAsync(textBox_URL.Text);
+
+            label_Durum.Text = "Video bilgisi çekiliyor";
+            foreach (var video in videos)
+            {
+                string vAdi = video.Title;
+                int vKalite = video.Resolution;
+                var vFormat = video.Format;
+
+                string SesDosya = "Ses [" + vKalite.ToString() + "] - " + vAdi;
+                string VideoDosya = "Video [" + vKalite.ToString() + "] - " + video.FullName;
+
+                if (FilterFormat.ToLower() == vFormat.ToString().ToLower() 
+                    && vKalite != -1
+                    && filterKalite.ToLower() == vKalite.ToString().ToLower())
+
+                {
+
+                    string cikti = string.Format("{0} - {1}, {2}", vKalite.ToString(), vKalite.ToString(), vFormat);
+                    listBox_Videolar.Items.Add(cikti);
+
+                    // Ses indir
+                    SesIndir(SesDosya);
+
+                    // Video indir
+                    var client = new HttpClient();
+                    long? totalByte = 0;
+                    using (Stream output = File.OpenWrite(Application.StartupPath + @"/Downloads/" + VideoDosya))
+                    {
+                        using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
+                        {
+                            totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+
+                        }
+                        using (var input = await client.GetStreamAsync(video.Uri))
+                        {
+                            byte[] buffer = new byte[16 * 1024];
+                            int read;
+                            int totalRead = 0;
+                            label_Durum.Text = "İndirme başladı";
+                            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                output.Write(buffer, 0, read);
+                                totalRead += read;
+                            }
+                            label_Durum.Text = "İndirme bitti";
+                        }
+                    }
+
+                }
+            }
+
+            label_Durum.Text = "Video bilgisi çekildi";
+        }
+
+
+
+        private async void SesIndir(string ad)
+        {
+            var youtube = YouTube.Default;
+            var video = await youtube.GetAllVideosAsync(textBox_URL.Text);
+            var audio = video
+            .Where(e => e.AudioFormat == AudioFormat.Aac && e.AdaptiveKind == AdaptiveKind.Audio)
+            .ToList();
+
+            string path = Application.StartupPath + @"/Downloads/";
+            string fileAAC = ad + ".aac";
+
+            string file = path + fileAAC;
+            if (audio.Count > 0)
+            {
+                byte[] bytes = audio[0].GetBytes();
+                File.WriteAllBytes(file, bytes);
+            }
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
